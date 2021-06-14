7b43b20d771c90547ef97b7f4f441752d3b1c617
xxKroliKxx <83514174+xxKroliKxx@users.noreply.github.com>
2021-06-14 21:53:33 +0500

    Add files via upload

--- logic.js
+++ logic.js
@@ -58,7 +58,7 @@ function CheckWinner() {
             return true;
         }
     };
-    
+
     count = 0;
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board.length; j++) {
